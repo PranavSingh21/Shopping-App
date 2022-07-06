56fdb1662d87ac8494f7d6fcdf4bd53a30051d39
PranavSingh21 <mailsandstuffs@gmail.com>
2022-07-06 14:50:24 +0530

    day-12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,17 +34,60 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => Itemwidget(
-                  item: CatalogModel.items[index],
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+
                 ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+
+                  child: GridTile(
+                    header: Container(child:
+                     Text(item.name,
+                    style: TextStyle(color: Colors.white),
+                    ),
+                    
+                    padding: const EdgeInsets.all(12),
+
+                    decoration: BoxDecoration(
+                      color:  Colors.blueGrey,
+
+                    ),),
+                    child: Image.network(item.image),
+                    footer: Container(
+                      child:
+                     Text(item.name,
+                    style: TextStyle(color: Colors.white),
+                    ),
+                    
+                    padding: const EdgeInsets.all(12),
+
+                    decoration: BoxDecoration(
+                      color:  Colors.blueGrey,
+
+                    ),
+
+                    
+                    )
+                   ) );
+                },
+                itemCount: CatalogModel.items.length,
               )
             : Center(
                 child: CircularProgressIndicator(),
